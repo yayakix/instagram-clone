8e298a8ceca4069847135b42e9ec1009c2c9e1af
Iyana Marquez <yayakix@gmail.com>
2022-05-30 20:42:46 -0400

    home page added seed data and basic styling

--- models/seed.js
+++ models/seed.js
@@ -2,22 +2,45 @@ module.exports = [
   {
     user: "Beans",
     caption: "A small pile of beans. Buy more beans for a big pile of beans.",
-    img: "https://imgur.com/LEHS8h3.png",
+    img: "https://images.pexels.com/photos/12070097/pexels-photo-12070097.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
     likes: 5,
     comments: ["hey", "love this!"],
   },
   {
     user: "bobber",
     caption: "A small pile of beans. Buy more beans for a big pile of beans.",
-    img: "https://imgur.com/LEHS8h3.png",
-    likes: 5,
+    img: "https://images.pexels.com/photos/11763347/pexels-photo-11763347.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    likes: 10,
     comments: ["hey", "d jeof!"],
   },
   {
     user: "Popper",
     caption: "A small pile of beans. Buy more beans for a big pile of beans.",
-    img: "https://imgur.com/LEHS8h3.png",
+    img: "https://images.pexels.com/photos/10242644/pexels-photo-10242644.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     likes: 5,
     comments: ["yo yo", "love this!"],
   },
+  {
+    user: "user23",
+    caption: "A small pile of beans. Buy more beans for a big pile of beans.",
+    img: "https://images.pexels.com/photos/11710126/pexels-photo-11710126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    likes: 8,
+    comments: ["yo yo", "love this!"],
+  },
+  {
+    user: "user553",
+    caption: "A small pile of beans. Buy more beans for a big pile of beans.",
+    img: "https://images.pexels.com/photos/12226848/pexels-photo-12226848.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    likes: 8,
+    comments: ["yo yo", "love this!"],
+  },
+  {
+    user: "uSERer23",
+    caption: "A small pile of beans. Buy more beans for a big pile of beans.",
+    img: "https://images.pexels.com/photos/12008109/pexels-photo-12008109.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    likes: 8,
+    comments: ["yo yo", "love this!"],
+  },
 ];
+
+
